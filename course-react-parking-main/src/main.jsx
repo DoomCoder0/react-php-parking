52d50f57ccbc7e217be37b1fdba91e05376a3af7
Bruno Obradović <156895426+DoomCoder0@users.noreply.github.com>
2024-12-11 12:38:27 +0100

    Add files via upload

---
+++ course-react-parking-main/src/main.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import axios from 'axios'
+import App from '@/App'
+import Home from '@/views/Home'
+import Register from '@/views/auth/Register'
+import Login from '@/views/auth/Login'
+import EditProfile from '@/views/profile/EditProfile'
+import ChangePassword from '@/views/profile/ChangePassword'
+import VehiclesList from '@/views/vehicles/VehiclesList'
+import CreateVehicle from '@/views/vehicles/CreateVehicle'
+import EditVehicle from '@/views/vehicles/EditVehicle'
+import ActiveParkings from '@/views/parkings/ActiveParkings'
+import ParkingHistory from '@/views/parkings/ParkingHistory'
+import OrderParking from '@/views/parkings/OrderParking'
+import ParkingDetails from '@/views/parkings/ParkingDetails'
+import '@/assets/main.css'
+import { route } from '@/routes'
+
+window.axios = axios
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+window.axios.defaults.withCredentials = true
+window.axios.defaults.baseURL = 'http://parkingapi.test/api/v1'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path={ route('home') } element={<App />}>
+          <Route index element={<Home />} />
+          <Route path={ route('register') } element={<Register />} />
+          <Route path={ route('login') } element={<Login />} />
+          <Route path={ route('profile.edit') } element={<EditProfile />} />
+          <Route path={ route('profile.change-password') } element={<ChangePassword />} />
+          <Route path={ route('vehicles.index') } element={<VehiclesList />} />
+          <Route path={ route('vehicles.create') } element={<CreateVehicle />} />
+          <Route path={ route('vehicles.edit') } element={<EditVehicle />} />
+          <Route path={ route('parkings.active') } element={<ActiveParkings />} />
+          <Route path={ route('parkings.history') } element={<ParkingHistory />} />
+          <Route path={ route('parkings.create') } element={<OrderParking />} />
+          <Route path={ route('parkings.show') } element={<ParkingDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
